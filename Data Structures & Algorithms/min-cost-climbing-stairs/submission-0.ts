823f452dfc582d20fcddd06f9ea2fe700761f7e9
Kashish Singh <54403943+singh-kashish@users.noreply.github.com>
2026-07-03 13:32:25 +0530

    Add: min-cost-climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-0.ts
@@ -0,0 +1,15 @@
+class Solution {
+    /**
+     * @param {number[]} cost
+     * @return {number}
+     */
+    minCostClimbingStairs(cost: number[]): number {
+        let dp = new Array(cost.length+1).fill(0);
+        dp[0]=0;
+        dp[1]=0
+        for(let i=2;i<=cost.length;i++){
+            dp[i]=Math.min(dp[i-1]+cost[i-1],dp[i-2]+cost[i-2]);
+        }
+        return Math.min(dp[cost.length]);
+    }
+}
