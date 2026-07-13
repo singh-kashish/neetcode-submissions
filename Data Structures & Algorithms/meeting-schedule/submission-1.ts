ea20e0a059843d3da71f64b4e615067b37509845
Kashish Singh <54403943+singh-kashish@users.noreply.github.com>
2026-07-13 13:50:30 +0530

    Add: meeting-schedule - submission-1

---
+++ Data Structures & Algorithms/meeting-schedule/submission-1.ts
@@ -0,0 +1,24 @@
+/**
+ * Definition of Interval:
+ * class Interval  {
+ *   constructor(start, end) {
+ *     this.start = start;
+ *     this.end = end;
+ *   }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {Interval[]} intervals
+     * @returns {boolean}
+     */
+    canAttendMeetings(intervals: Interval[]): boolean {
+        intervals.sort((a,b)=>a.start-b.start);
+        console.log(intervals)
+        for(let i=0;i<intervals.length-1;i++){
+            if(intervals[i].end>intervals[i+1].start)return false;
+        }
+        return true;
+    }
+}
