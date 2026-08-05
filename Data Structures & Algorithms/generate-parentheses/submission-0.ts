c34640b051281ae7eecd2fcc39c0df9cfb931043
Kashish Singh <54403943+singh-kashish@users.noreply.github.com>
2026-08-05 17:25:30 +0530

    Add: generate-parentheses - submission-0

---
+++ Data Structures & Algorithms/generate-parentheses/submission-0.ts
@@ -0,0 +1,33 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @return {string[]}
+     */
+    generateParenthesis(n: number): string[] {
+        // add to result array if openCount == closeCount == n
+        // add '(' if open<close
+        // add ')' if open>close
+        const stack = new Array<string>();
+        const res = new Array<string>();
+        function backtrack(open,close){
+            if(open==close && open==n){
+                res.push(`${[...stack].join('')}`)
+                return
+            }
+
+            if(open<n){
+                stack.push('(')
+                backtrack(open+1,close)
+                stack.pop()
+            }
+
+            if(open>close){
+                stack.push(')')
+                backtrack(open,close+1)
+                stack.pop();
+            }
+        }
+        backtrack(0,0)
+        return res;
+    }
+}
