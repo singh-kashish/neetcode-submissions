652f3b1dbb1e6298dda5d3a232a8135d1f1c0356
Kashish Singh <54403943+singh-kashish@users.noreply.github.com>
2026-08-23 17:11:46 +0530

    Add: combinations-of-a-phone-number - submission-0

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-0.ts
@@ -0,0 +1,34 @@
+class Solution {
+    /**
+     * @param {string} digits
+     * @return {string[]}
+     */
+    letterCombinations(digits: string): string[] {
+        let res = new Array<string>();
+        const n=digits.length
+        const obj :Record<string,string[]> = {
+            2:['a','b','c'],
+            3:['d','e','f'],
+            4:['g','h','i'],
+            5:['j','k','l'],
+            6:['m','n','o'],
+            7:['p','q','r','s'],
+            8:['t','u','v'],
+            9:['w','x','y','z']
+        }
+        if(n==0)return[]
+        function backtrack(i,str):void{
+            if(i>=n){
+                res.push(str)
+                return
+            }
+            for(let x=0;x<obj[digits[i]].length;x++){
+                str+=obj[digits[i]][x]
+                backtrack(i+1,str)
+                str=str.slice(0,-1)
+            }
+        }
+        backtrack(0,"")
+        return res;
+    }
+}
